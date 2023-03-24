81df430811e7a242fe628164c363f058dd780d1f
Carlos E. Evangelista <kaiunido@gmail.com>
2023-03-24 00:25:56 -0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-navigation-drawer :rail="drawer" :expand-on-hover="drawer">
+      <v-list density="compact" nav>
+        <v-list-item
+          prepend-icon="mdi-folder"
+          title="Home"
+          to="/"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar order="-1">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-uppercase">Tec Company</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const drawer = ref(false);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="bg-blue-grey-lighten-5 h-100" fluid>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-uppercase">Index Page</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped></style>
